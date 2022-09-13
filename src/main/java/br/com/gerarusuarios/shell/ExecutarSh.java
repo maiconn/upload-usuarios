d6947f975f0cf4e213c7346ef462bfb127ec824c
Maicon Gerardi <maicon.gerardi@gmail.com>
2022-09-13 00:17:47 -0300

    ajustar automaticamente o OpenApiConfig

--- src/main/java/br/com/gerarusuarios/shell/ExecutarSh.java
+++ src/main/java/br/com/gerarusuarios/shell/ExecutarSh.java
@@ -7,17 +7,8 @@ public class ExecutarSh {
     public static void executarCriacaoUserJenkins(String user, String senha, String email) throws IOException, InterruptedException {
         ProcessBuilder builder = new ProcessBuilder();
         builder.command("sudo",
-                "java",
-                "-jar",
-                "/usr/local/bin/jenkins_user/jenkins-cli.jar",
-                "-s",
-                "http://localhost:8080/",
-                "-auth",
-                "admin:11f9d8012168e0c6b670fba5400153c4e0",
-                "groovy",
-                "=",
-                "<",
-                "/usr/local/bin/jenkins_user/user-creation.groovy",
+                "sh",
+                "/usr/local/bin/jenkins_user/create-user.sh",
                 user,
                 senha,
                 email);
